b99c97ffbf191fa9237ab65646a1705396608672
Chris Barnett <Christopher.Barnett@tufts.edu>
2026-04-16 11:34:54 -0400

    make sure units_to_bytes returns int add some allocation utils

--- coldfront_utils/util/units.py
+++ coldfront_utils/util/units.py
@@ -1,7 +1,7 @@
 
 
 
-def bytes_to_units(value, units="TB"):
+def bytes_to_units(value, units="TB") -> float:
     units = units.lower().strip()
     if units == "tb":
         return value / (1000 ** 4)
@@ -15,15 +15,15 @@ def bytes_to_units(value, units="TB"):
         raise ValueError(f"Unrecognized storage unit '{units}'")
 
 
-def units_to_bytes(value, units="TB"):
+def units_to_bytes(value, units="TB") -> int:
     units = units.lower().strip()
     if units == "tb":
-        return value * (1000 ** 4)
+        return int(value * (1000 ** 4))
     elif units == "gb":
-        return value * (1000 ** 3)
+        return int(value * (1000 ** 3))
     elif units == "tib":
-        return value * (1024 ** 4)
+        return int(value * (1024 ** 4))
     elif units == "gib":
-        return value * (1024 ** 3)
+        return int(value * (1024 ** 3))
     else:
         raise ValueError(f"Unrecognized storage unit '{units}'")
